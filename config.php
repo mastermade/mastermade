5c014cf76ea79073db99d66d337d178b97d3f8b4
Morgante Pell <morgante.pell@morgante.net>
2012-08-11 02:18:55 -0400

    create habaru config

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+Config::set( 'db_connection', array(
+        'connection_string'=>'sqlite:habari.db',
+        'username'=>'',
+        'password'=>'',
+        'prefix'=>'habari__'
+));
+
+//$locale = 'en-us';
+?>
