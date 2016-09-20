cbb7097ab24775ca6cc2ecd709e3d817e92c1ba9
Morgante Pell <morgante.pell@morgante.net>
2016-09-20 17:58:09 -0400

    Work on buildability

--- web_modules/layouts/Homepage/index.js
+++ web_modules/layouts/Homepage/index.js
@@ -3,7 +3,9 @@ import { BodyContainer } from 'phenomic';
 import { connect } from 'react-redux';
 
 import Page from '../Page';
-import Grid from '../../gridgame/Grid';
+
+const isBrowser = typeof window !== 'undefined';
+const Grid = isBrowser ? require('../../gridgame/Grid') : null;
 
 class Homepage extends Component {
   static contextTypes = {
@@ -13,10 +15,12 @@ class Homepage extends Component {
   render() {
     const { body } = this.props;
 
+    const background = isBrowser ? (<Grid cellSize={10} />) : null;
+
     return (
       <Page {...this.props}>
         <BodyContainer>{ body }</BodyContainer>
-        <Grid cellSize={10} />
+        {background}
       </Page>
     );
   }
