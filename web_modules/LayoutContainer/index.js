4144555d23f301506e8c3120d6de325857f2de34
Morgante Pell <morgante.pell@morgante.net>
2016-09-21 01:04:01 -0400

    Make active styles on layout container work properly and excellently

--- web_modules/LayoutContainer/index.js
+++ web_modules/LayoutContainer/index.js
@@ -3,6 +3,9 @@ import Helmet from 'react-helmet';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Link from 'phenomic/lib/Link';
+import { connect } from 'react-redux';
+import classNames from 'classnames';
+import * as _ from 'lodash';
 
 // Import global CSS before other components and their styles
 import './index.global.css';
@@ -13,9 +16,9 @@ import Footer from '../Footer';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
+// injectTapEventPlugin();
 
-export default class Layout extends Component {
+class Layout extends Component {
 
   static propTypes = {
     children: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
@@ -30,7 +33,13 @@ export default class Layout extends Component {
       pkg,
     } = this.context.metadata;
 
-    console.log('context', this.context, this.props);
+    const activePage = this.props.params.splat;
+
+    const pages = [
+      { id: 'about', title: 'About' },
+      { id: 'services', title: 'Services' },
+      { id: 'portfolio', title: 'Portfolio' },
+    ];
 
     return (
       <MuiThemeProvider>
@@ -58,13 +67,19 @@ export default class Layout extends Component {
           <style>{ "@-ms-viewport { width: device-width; }" }</style>
 
           <div className={styles.tilepage}>
-            <div className={styles.home}>MasterMade</div>
-            <div className={styles.navigation}>
-              <div className={styles.about}>
-               <Link to={"/about/"}>About</Link>
-              </div>
-              <div className={styles.services}>Services</div>
-              <div className={styles.portfolio}>Portfolio</div>
+            <div className={styles.home}><Link to={"/"}>MasterMade</Link></div>
+            <div className={classNames(styles.navigation, { [styles.active]: (activePage !== '') })}>
+              { _.map(pages, (page) => {
+                const className = classNames(styles[page.id], {
+                  [styles.active]: (activePage === page.id),
+                });
+
+                return (
+                  <div className={className}>
+                    <Link to={`/${page.id}/`}>{ page.title }</Link>
+                  </div>
+                );
+              }) }
             </div>
           </div>
       </div>
@@ -72,3 +87,5 @@ export default class Layout extends Component {
     );
   }
 }
+
+export default Layout;
